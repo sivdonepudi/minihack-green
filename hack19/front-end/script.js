f0aadf433d76b49dd50dce0242b334e36d624be5
Petar Dimov <peterpcm3@gmail.com>
2022-05-31 21:10:05 +0100

    initial images colour picker

--- hack19/front-end/script.js
+++ hack19/front-end/script.js
@@ -1,12 +1,29 @@
+//var capAllocatedColours = [];
+
 const colorPicker = document.querySelector(".colorPicker");
-console.log(colorPicker);
+
 // colorPicker.addEventListener("input", updateFirst, false);
 colorPicker.addEventListener("change", watchColorPicker, false);
 
 function watchColorPicker(event) {
+  console.log(event);
+
   document.querySelectorAll(".kit-section").forEach((kitSection) => {
-    // if (event.target.name === "cap");
-    kitSection.style.backgroundColor = event.target.value;
-    // console.log(event.target.value);
+    console.log(kitSection.classList);
+    if(event.target.name == 'cap' && kitSection.classList.contains("cap")) {
+      // if (event.target.name === "cap");
+      //if (capAllocatedColours.indexOf(event.target.value) > -1) {
+      kitSection.style.backgroundColor = event.target.value;
+      //capAllocatedColours.push = event.target.value;
+    }
+    else if(event.target.name == 'jersey' && kitSection.classList.contains("jersey")) {
+      kitSection.style.backgroundColor = event.target.value;
+    }
+    else if(event.target.name == 'trousers' && kitSection.classList.contains("trousers")) {
+      kitSection.style.backgroundColor = event.target.value;
+    }
+    else if(event.target.name == 'saddle' && kitSection.classList.contains("saddle")) {
+      kitSection.style.backgroundColor = event.target.value;
+    }
   });
 }
